28825571ff62215b3a417559f17a805407fc794b
Israel <iecoding@gmail.com>
2025-12-28 18:49:39 -0600

    Enhance Vite configuration by adding custom server headers for caching control and enabling dependency optimization.

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,15 @@ import mkcert from 'vite-plugin-mkcert';
 
 export default defineConfig({
     plugins: [mkcert()],
-    server: {https: true},
+    server: {
+        https: true,
+        headers: {
+            'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate',
+            'Pragma': 'no-cache',
+            'Expires': '0'
+        }
+    },
+    optimizeDeps: {
+        force: true
+    }
 });
